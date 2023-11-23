8952c93ad5d0d2a222b940c5d0caea9506e53582
Arminchiks <firedemons51@gmail.com>
2023-11-23 14:37:25 +0200

    Update tictac.py
    3 stunda

--- tictac.py
+++ tictac.py
@@ -12,15 +12,17 @@ def btnClick(button): #padod visu pogu
         button["text"]="X"#maina uz X
         speletajsX=False
         count+=1#palielina rutinu skaitu
+        checkWinner()
        
     elif button['text']==''and speletajsX==False: #mainās spēlētāji
         button['text']='O'
         speletajsX=True
         count+=1
+        checkWinner()
        
     else:
         messagebox.showerror("Tictactoe","Šeit jau kāds ir uzlicis")
-    return
+    return 0
 
 
 btn1=Button(mansLogs,text='',width=6,height=3,font=("Helvica", 24),command=lambda:btnClick(btn1))
@@ -45,21 +47,91 @@ btn7.grid(row=2,column=0)
 btn8.grid(row=2,column=1)
 btn9.grid(row=2,column=2)
 
+def disableButtons():
+    btn1.config(state=DISABLED)
+    btn2.config(state=DISABLED)
+    btn3.config(state=DISABLED)
+    btn4.config(state=DISABLED)
+    btn5.config(state=DISABLED)
+    btn6.config(state=DISABLED)
+    btn7.config(state=DISABLED)
+    btn8.config(state=DISABLED)
+    btn9.config(state=DISABLED)
+    return 0
 
+def reset():
+    btn1.config(state=NORMAL)
+    btn2.config(state=NORMAL)
+    btn3.config(state=NORMAL)
+    btn4.config(state=NORMAL)
+    btn5.config(state=NORMAL)
+    btn6.config(state=NORMAL)
+    btn7.config(state=NORMAL)
+    btn8.config(state=NORMAL)
+    btn9.config(state=NORMAL)
+    btn1['text']=''
+    btn2['text']=''
+    btn3['text']=''
+    btn4['text']=''
+    btn5['text']=''
+    btn6['text']=''
+    btn7['text']=''
+    btn8['text']=''
+    btn9['text']=''
+    count=0 
 
 def checkWinner():
     global winner
-    winner==False #noteiks ja bus neizskirts
-    if (btn1["text"]=='X' and btn2["text"]=='X'and btn3["text"=='X'] or btn1["text"]=='X' and btn4["text"]=='X'and btn7["text"=='X']or btn1["text"]=='X' and btn5["text"]=='X'and btn9["text"=='X'] or btn2["text"]=='X' and btn5["text"]=='X'and btn8["text"=='X'] or btn3["text"]=='X' and btn5["text"]=='X'and btn7["text"=='X'] or btn3["text"]=='X' and btn6["text"]=='X'and btn9["text"=='X'] or btn4["text"]=='X' and btn5["text"]=='X'and btn6["text"=='X'] or btn7["text"]=='X' and btn8["text"]=='X'and btn9["text"=='X']):
+    winner=False #noteiks ja bus neizskirts
+    if (btn1["text"]=='X' and btn2["text"]=='X'and btn3["text"]=='X' or btn1["text"]=='X' and btn4["text"]=='X'and btn7["text"]=='X'or btn1["text"]=='X' and btn5["text"]=='X'and btn9["text"]=='X' or btn2["text"]=='X' and btn5["text"]=='X'and btn8["text"]=='X' or btn3["text"]=='X' and btn5["text"]=='X'and btn7["text"]=='X' or btn3["text"]=='X' and btn6["text"]=='X'and btn9["text"]=='X' or btn4["text"]=='X' and btn5["text"]=='X'and btn6["text"]=='X' or btn7["text"]=='X' and btn8["text"]=='X'and btn9["text"]=='X'):
         winner==True
-       
+        disableButtons()
         messagebox.showinfo("Tictactoe","SpeletajsX ir uzvarētājs")
-    elif (btn1["text"]=='O' and btn2["text"]=='O'and btn3["text"=='O'] or btn1["text"]=='O' and btn4["text"]=='O'and btn7["text"=='O']or btn1["text"]=='O' and btn5["text"]=='O'and btn9["text"=='O'] or btn2["text"]=='O' and btn5["text"]=='O'and btn8["text"=='O'] or btn3["text"]=='O' and btn5["text"]=='O'and btn7["text"=='O'] or btn3["text"]=='O' and btn6["text"]=='O'and btn9["text"=='O'] or btn4["text"]=='O' and btn5["text"]=='O'and btn6["text"=='O'] or btn7["text"]=='O' and btn8["text"]=='O'and btn9["text"=='O']):
-        winner==False
     
+    elif (btn1["text"]=='O' and btn2["text"]=='O'and btn3["text"]=='O' or btn1["text"]=='O' and btn4["text"]=='O'and btn7["text"]=='O'or btn1["text"]=='O' and btn5["text"]=='O'and btn9["text"]=='O' or btn2["text"]=='O' and btn5["text"]=='O'and btn8["text"]=='O' or btn3["text"]=='O' and btn5["text"]=='O'and btn7["text"]=='O' or btn3["text"]=='O' and btn6["text"]=='O'and btn9["text"]=='O' or btn4["text"]=='O' and btn5["text"]=='O'and btn6["text"]=='O' or btn7["text"]=='O' and btn8["text"]=='O'and btn9["text"]=='O'):
+        winner=True
+        disableButtons()
         messagebox.showinfo("Tictactoe","SpeletajsO ir uzvarētājs")
+    
     elif count==9 and winner==False:
         messagebox.showinfo("Tictactoe","Neizšķirts")
+        disableButtons()
+
+
+#def infoLogs():
+   # jaunsLogs=Toplevel()
+   # jaunsLogs
+
+
+
+galvenaIzvelne=Menu(mansLogs)#izveido galveno izvēlni
+mansLogs.config(menu=galvenaIzvelne)#pievieno galvenajam logam 
+opcijas=Menu(galvenaIzvelne,tearoff=False)#maza izvēlne
+galvenaIzvelne.add_cascade(label="Opcijas",menu=opcijas)   
+#lejupkritosais sarakst
+opcijas.add_command(label="Jauna spēle",command=reset)
+opcijas.add_command(label="Iziet",command=mansLogs.quit)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
